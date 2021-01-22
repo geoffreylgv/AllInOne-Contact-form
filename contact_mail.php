403dadddbcef2160250bbe4b8e08ef96a081489c
Geoffrey Logovi <52314615+geoffreylgv@users.noreply.github.com>
2021-01-22 14:41:40 +0000

    Add files via upload

---
+++ contact_mail.php
@@ -0,0 +1,9 @@
+<?php
+$toEmail = "zixgeoffrey2@gmail.com";
+$mailHeaders = "From: " . $_POST["userName"] . "<". $_POST["userEmail"] .">\r\n";
+if(mail($toEmail, $_POST["subject"], $_POST["content"], $mailHeaders)) {
+print "<p class='is-success'>Mail envoyé avec succès.</p>";
+} else {
+print "<p class='is-danger'>Une erreur est survenu lor de l'envoi, réessayer !</p>";
+}
+?>
