5dd3590f16f085d666a53fd12ae6c18fd4d6fb8d
geoffreylgv <evanomarkez@gmail.com>
2022-10-31 21:20:55 +0000

    update factorising like firstly

--- contact_mail.php
+++ contact_mail.php
@@ -16,6 +16,16 @@ $bodySuccessText = '<div class="tata success slide-right-in top-right" id="tata-
     <div class="tata-progress" style="animation: 10s linear 0s 1 normal forwards running reduceWidth;"></div>
   </div>';
 
+  $bodyErrorText = '<div class="tata error slide-right-in top-right" id="tata-1611852257032">
+  <i class="tata-icon material-icons">block</i>
+  <div class="tata-body">
+  <h4 class="tata-title">Erreur</h4>
+  <p class="tata-text">Une erreur est survenu lor de l\'envoi, réessayer !</p>
+  </div>
+  <button class="tata-close material-icons">clear</button>
+  <div class="tata-progress" style="animation: 10s linear 0s 1 normal forwards running reduceWidth;"></div>
+  </div>';
+
 $mail_msg = " 
             <h3 style='text-align:center;'>Hi Geoffrey, Message from <a href='mailto:$userMail'>$userMail</a>.</h3>
             <p style='text-align:center;'><b>Subject: $userSubject</b></p>
@@ -25,16 +35,6 @@ $mail_msg = "
 if (mail($toEmail, $userSubject, $mail_msg, $mailHeaders)) {
   echo $bodyText;
 } else {
-  echo '
-  <div class="tata error slide-right-in top-right" id="tata-1611852257032">
-  <i class="tata-icon material-icons">block</i>
-  <div class="tata-body">
-  <h4 class="tata-title">Erreur</h4>
-  <p class="tata-text">Une erreur est survenu lor de l\'envoi, réessayer !</p>
-  </div>
-  <button class="tata-close material-icons">clear</button>
-  <div class="tata-progress" style="animation: 10s linear 0s 1 normal forwards running reduceWidth;"></div>
-  </div>
-  ';
+  echo $bodyErrorText;
 }
 
